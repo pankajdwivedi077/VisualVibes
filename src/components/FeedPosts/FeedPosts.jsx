e572cf6822c32c2e6c6a9322e1b73a05ccc817a6
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-02-07 16:23:27 +0530

    postfeed

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -4,10 +4,26 @@ import FeedPost from './FeedPost'
 const FeedPosts = () => {
   return (
     <Container maxW={"container.sm"} py={10} px={2} >
-       <FeedPost />
-       <FeedPost />
-       <FeedPost />
-       <FeedPost />
+       <FeedPost 
+       img='/img2.jpg'
+       username='eva'
+       avatar='/img2.jpg'
+       />
+        <FeedPost 
+       img='/img4.jpg'
+       username='alice'
+       avatar='/img4.jpg'
+       />
+         <FeedPost 
+       img='/img3.jpg'
+       username='lisa'
+       avatar='/img3.jpg'
+       />
+         <FeedPost 
+       img='/img1.jpg'
+       username='lana'
+       avatar='/img1.jpg'
+       />
     </Container>
   )
 }
--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,17 +1,70 @@
-import { Box, Flex } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { CommentLogo, NotificationsLogo, UnlikeLogo } from '../../assets/contants'
 
-const PostFooter = () => {
+const PostFooter = ({ username }) => {
 
-  
+    const [liked, setLiked] = useState(false);
+	const [likes, setLikes] = useState(1000);
+
+	const handleLike = () => {
+		if (liked) {
+			setLiked(false);
+			setLikes(likes - 1);
+		} else {
+			setLiked(true);
+			setLikes(likes + 1);
+		}
+	};
 
   return (
     <>
-     <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={'auto'} > 
-       <Box>
-
+     <Box my={10}></Box>
+     <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4} > 
+       <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
+         {!liked ? (<NotificationsLogo />) : (<UnlikeLogo />)}
+       </Box>
+       <Box cursor={"pointer"} fontSize={18}>
+        <CommentLogo />
        </Box>
      </Flex>
+     <Text fontWeight={600} fontSize={"sm"}>
+        {likes} likes
+     </Text>
+
+     <Text fontWeight={700} fontSize={"sm"}>
+       {username} {" "}
+       <Text as='span' fontWeight={400}>
+        Feeling good
+       </Text>
+     </Text>
+     <Text color={"gray"} fontSize={"sm"}>
+        view all comments
+     </Text>
+
+     <Flex
+      alignItems={"center"}
+      gap={2}
+      justifyContent={"space-between"}
+      w={"full"}
+     > 
+     <InputGroup>
+       <Input variant={"flushed"} placeholder={'Add a comment...'} fontSize={14} />
+       <InputRightElement>
+        <Button 
+         fontSize={14}
+         color={'blue.500'}
+         fontWeight={600}
+         cursor={"pointer"}
+         _hover={{color: "white"}}
+         bg={"transparent"}
+        >
+            Post
+        </Button>
+       </InputRightElement>
+     </InputGroup>
+     </Flex>
+
     </>
   )
 }
