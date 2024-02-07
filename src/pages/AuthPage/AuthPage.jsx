83e6e54d3cfc51d7062c3e85899d0644287ef980
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-02-07 12:18:29 +0530

    Auth form

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,21 +1,29 @@
-import { Box, Container, Flex, Image, VStack } from '@chakra-ui/react'
+import { Box, Container, Flex, Image, VStack } from "@chakra-ui/react";
+import AuthForm from "../../components/AuthForm/AuthForm";
 
 const AuthPage = () => {
   return (
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
-        <Container maxWidth={Container.md} padding={0} >
-            {/* Left hand-side */}
-            <Box display={{base:"none", md:"block"}}>
-               <Image src='/auth.jpg' h={550} alt='Phone img'/>
-            </Box>
+      <Container maxWidth={Container.md} padding={0}>
+        <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+          {/* Left hand-side */}
+          <Box display={{ base: "none", md: "block" }}>
+            <Image src="/auth.jpg" h={420} alt="Phone img" />
+          </Box>
 
-            {/* Right hand-side */}
-           <VStack spacing={4} >
-
-           </VStack>
-        </Container>
+          {/* Right hand-side */}
+          <VStack spacing={4} align={"stretch"}>
+            <AuthForm />
+            <Box textAlign={"center"}>Get the app.</Box>
+            <Flex gap={5} justify={"center"}>
+              <Image src="/playstore.png" h={10} alt="PlayStore logo" />
+              <Image src="/microsoft.png" h={10} alt="Microsoft logo" />
+            </Flex>
+          </VStack>
+        </Flex>
+      </Container>
     </Flex>
-  )
-}
+  );
+};
 
-export default AuthPage
+export default AuthPage;
