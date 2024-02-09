47e34f86d782cdc186b5a4c6ff86dfe4f7b7f22e
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-02-09 20:08:26 +0530

    Login and Signup with firebase

---
+++ src/components/AuthForm/Signup.jsx
@@ -0,0 +1,68 @@
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { Input, Button, InputGroup, InputRightElement } from "@chakra-ui/react";
+import { useState } from "react";
+
+const Signup = () => {
+
+    const [inputs, setInputs] = useState({
+        fullname: "",
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const [showPassword, setShowPassword] = useState(false);
+      
+  return (
+    <>
+        <Input 
+          placeholder='Email'
+          fontSize={14}
+          type='email'
+          value={inputs.email}
+          size={"sm"}
+          onChange={(e) => setInputs({...inputs, email: e.target.value})}
+        />
+        <Input 
+          placeholder='Username'
+          fontSize={14}
+          type='text'
+          value={inputs.username}
+          size={"sm"}
+          onChange={(e) => setInputs({...inputs, username: e.target.value})}
+        />
+        <Input 
+          placeholder='Full Name'
+          fontSize={14}
+          type='text'
+          value={inputs.fullname}
+          size={"sm"}
+          onChange={(e) => setInputs({...inputs, fullname: e.target.value})}
+        />
+       <InputGroup>
+       <Input 
+          placeholder='Password'
+          fontSize={14}
+          type={showPassword ? "text" : "password"}
+          value={inputs.password}
+          size={"sm"}
+          onChange={(e) => setInputs({...inputs, password: e.target.value})}
+        />
+
+       <InputRightElement h="full">
+         <Button variant={"ghost"} size={"sm"} onClick={()=>setShowPassword(!showPassword)}>
+            {showPassword ? <ViewIcon /> : <ViewOffIcon /> }
+         </Button>
+       </InputRightElement>
+
+       </InputGroup>
+
+       <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14}   >
+         Sign up
+        </Button>
+
+    </>
+  )
+}
+
+export default Signup
