4e1b56c71f30fd0d3107f42b08604237c7488d43
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-02-11 17:57:36 +0530

    login with google

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -22,7 +22,7 @@ const AuthForm = () => {
           <Box flex={2} h={"1px"} bg={"gray.400"} />
         </Flex>
 
-        <GoogleAuth />
+        <GoogleAuth prefix={isLogin ? "Log in" : "Sign up"} />
 
       </VStack>
      </Box>
--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -1,11 +1,59 @@
 import { Flex, Text, Image } from "@chakra-ui/react"; 
+import { useSignInWithGoogle } from "react-firebase-hooks/auth"; 
+import { auth, firestore } from "../../firebase/firebase"; 
+import useShowToast from "../../hooks/useShowToast";
+import useAuthStore from "../../store/authStore";
+import {  doc, getDoc, setDoc } from "firebase/firestore";
 
-const GoogleAuth = () => {
+const GoogleAuth = ({ prefix }) => {
+  const [signInWithGoogle, , , error] = useSignInWithGoogle(auth);
+  const showToast = useShowToast();
+  const loginUser = useAuthStore((state) => state.login);
+
+  const handleGoogleAuth = async () => {
+     try {
+       const newUser = await signInWithGoogle()
+       if (!newUser && error){
+        showToast("Error",error.message,"error")
+        return;
+       }
+
+      const userRef = doc(firestore, "users", newUser.user.uid);
+      const userSnap = await getDoc(userRef);
+
+       if(userSnap.exists()){
+        //  login
+        const userDoc = userSnap.data();
+        localStorage.setItem("user-info", JSON.stringify(userDoc));
+        loginUser(userDoc);
+       }else {
+        // signup
+        const userDoc = {
+          uid: newUser.user.uid,
+          email: newUser.user.email,
+          username: newUser.user.email.split("@")[0],
+          fullName: newUser.user.displayName,
+          bio: "",
+          profilePicUrl: newUser.user.photoURL,
+          followers: [],
+          folling: [],
+          posts: [],
+          createdAt: Date.now(),
+        };
+        await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
+        localStorage.setItem("user-info", JSON.stringify(userDoc));
+        loginUser(userDoc);
+       }
+     } catch (error) {
+      showToast("Error", error.message, "error")
+     }
+  }
+
   return (
-    <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
+    <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"} onClick={handleGoogleAuth}>
       <Image src="/google.png" w={5} alt="Google" />
       <Text mx="2" color={"blue.500"}>
-        Log in with Google
+        {prefix} with Google
       </Text>
     </Flex>
   );
