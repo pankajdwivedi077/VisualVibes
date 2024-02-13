c458ed4bc530be5cefe43b140e41f3f1b8a78e15
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-02-13 14:32:34 +0530

    sidebar update

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,36 +1,14 @@
-import { Avatar, Box, Button, Flex, Link, Tooltip } from '@chakra-ui/react';
+import {  Box, Button, Flex, Link, Tooltip } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
-import { CreatePostLogo, InstagramLogo, InstagramMobileLogo, NotificationsLogo, SearchLogo } from '../../assets/contants';
-import { AiFillHome } from 'react-icons/ai';
+import {  InstagramLogo, InstagramMobileLogo } from '../../assets/contants';
+
 import { BiLogOut } from 'react-icons/bi'
 import useLogout from '../../hooks/useLogout';
+import SidebarItems from './SidebarItems';
 
 const Sidebar = () => {
 
- const sidebarItems = [
-  {
-    icon: <AiFillHome size={25} />,
-    text: 'Home',
-    link: "/",
-  },
-  {
-    icon: <SearchLogo />,
-    text: "Search",
-  },
-  {
-    icon: <NotificationsLogo />,
-    text: "Notifications",
-  },
-  {
-    icon: <CreatePostLogo />,
-    text: "create",
-  },
-  {
-    icon: <Avatar size={"sm"} name='Burak Orkmez' src='/logo.png' />,
-    text: 'Profile',
-    link: "/visualvibes",
-  },
- ];
+ 
 
   const {handleLogout, isLoggingOut} = useLogout()
 
@@ -53,35 +31,7 @@ const Sidebar = () => {
       <InstagramMobileLogo />
     </Link>
     <Flex direction={"column"} gap={5} cursor={"pointer"}>
-        {sidebarItems.map((item, index) => (
-          <Tooltip
-           key={index}
-           hasArrow
-           label={item.text}
-           placement='right'
-           ml={1}
-           openDelay={500}
-           display={{base: "block", md: 'none'}}
-          >
-           <Link
-            display={"flex"}
-            to={item.link || null}
-            as={RouterLink}
-            alignItems={"center"}
-            gap={4}
-            _hover={{bg: "whiteAlpha.400"}}
-            borderRadius={6}
-            p={2}
-            w={{base:10,md:"full"}}
-            justifyContent={{base: "center",md:"flex-start"}}
-            >
-           {item.icon}
-           <Box display={{base:"none",md:"block"}}>
-            {item.text}
-           </Box>
-           </Link>
-          </Tooltip>
-        ))}
+       <SidebarItems />
     </Flex>
 
     {/* Logout */}
