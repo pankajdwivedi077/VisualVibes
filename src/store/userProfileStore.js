87315e497847b62274f9f3746c17f711a544dd88
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-02-13 17:30:26 +0530

    createPost

--- src/store/userProfileStore.js
+++ src/store/userProfileStore.js
@@ -3,7 +3,9 @@ import { create } from "zustand";
 const useUserprofileStore = create((set) => ({
     userProfile:null,
     setUserProfile:(userProfile) => set({ userProfile }),
-    // addPost:()
+    addPost: (post) => set(state => ({
+        userProfile:{...state.userProfile,posts:[post.id,...state.userProfile.posts]}
+    }))
 }))
 
 export default useUserprofileStore;
